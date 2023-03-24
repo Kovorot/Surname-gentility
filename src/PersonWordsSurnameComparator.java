4a892fc93c6b0e6a5cc94ab9403b17f450028f76
Andrey <kovorot0875@mail.ru>
2023-03-24 17:24:21 +0300

    Comparator class added

---
+++ src/PersonWordsSurnameComparator.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+public class PersonWordsSurnameComparator implements Comparator<Person> {
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        String[] parts1 = o1.getSurname().split(" ");
+        String[] parts2 = o2.getSurname().split(" ");
+        if (parts1.length > parts2.length) {
+            return 1;
+        } else if (parts1.length < parts2.length) {
+            return -1;
+        } else {
+            return Integer.compare(o1.getAge(), o2.getAge());
+        }
+    }
+}
