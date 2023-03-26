833422bbac306296c8ffa9b7353e279b8f8355e8
Andrey <kovorot0875@mail.ru>
2023-03-26 19:48:38 +0300

    Young people added

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,15 @@ public class Main {
 
         List<Person> people = new ArrayList<>();
 
-        people.add(new Person("Петр", "Васильевич", 34));                    //7
+        people.add(new Person("Петр", "Васильевич", 34));                    //8
         people.add(new Person("Рамзан", "Хамид Оглы Ахмед", 39));            //1
         people.add(new Person("Назар", "Аллаз Оглы", 17));                   //5
         people.add(new Person("Рамиль", "Мамед Ахмед Багет", 21));           //3 (Я устал придумывать длинные фамилии)
         people.add(new Person("Ахмед", "Исмаил Отар", 58));                  //4
-        people.add(new Person("Иван", "Вячеславович", 39));                  //6
+        people.add(new Person("Иван", "Вячеславович", 39));                  //7
         people.add(new Person("Рамзес", "Хатам Ибрагим Ахмед Магомед", 25)); //2
+        people.add(new Person("Оксана", "Вячеславовна", 17));                //9
+        people.add(new Person("Владимир", "Фон Барон", 8));                  //6
 
         Collections.sort(people, new PersonWordsSurnameComparator(3));
 
