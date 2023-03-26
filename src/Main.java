4547fa8dac328bc8e6d05111a656108c95eedc6c
Andrey <kovorot0875@mail.ru>
2023-03-26 19:28:27 +0300

    Lambda added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -15,8 +16,22 @@ public class Main {
         people.add(new Person("Иван", "Вячеславович", 39));                  //6
         people.add(new Person("Рамзес", "Хатам Ибрагим Ахмед Магомед", 25)); //2
 
-        Collections.sort(people, new PersonWordsSurnameComparator(3));
+        Comparator<Person> comparator = (o1, o2) -> {
+            int maxWordAmount = 3;
+            String[] parts1 = o1.getSurname().split(" ");
+            String[] parts2 = o2.getSurname().split(" ");
+            if (parts1.length >= maxWordAmount && parts2.length >= maxWordAmount) {
+                return (Integer.compare(o1.getAge(), o2.getAge())) * -1;
+            } else if (parts1.length > parts2.length) {
+                return -1;
+            } else if (parts1.length < parts2.length) {
+                return 1;
+            } else {
+                return (Integer.compare(o1.getAge(), o2.getAge())) * -1;
+            }
+        };
 
+        Collections.sort(people, comparator);
         System.out.println(people);
     }
 }
