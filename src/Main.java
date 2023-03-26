aebfcd72f3b5984fb39c3b020d5cd74f6aebd8bf
Andrey <kovorot0875@mail.ru>
2023-03-26 20:11:48 +0300

    Remove system added

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,18 @@ public class Main {
 
         people.add(new Person("Петр", "Васильевич", 34));                    //8
         people.add(new Person("Рамзан", "Хамид Оглы Ахмед", 39));            //1
-        people.add(new Person("Назар", "Аллаз Оглы", 17));                   //5
-        people.add(new Person("Рамиль", "Мамед Ахмед Багет", 21));           //3 (Я устал придумывать длинные фамилии)
+        people.add(new Person("Назар", "Аллаз Оглы", 17));                   //5 will be removed
+        people.add(new Person("Рамиль", "Мамед Ахмед Багет", 21));           //3
         people.add(new Person("Ахмед", "Исмаил Отар", 58));                  //4
         people.add(new Person("Иван", "Вячеславович", 39));                  //7
         people.add(new Person("Рамзес", "Хатам Ибрагим Ахмед Магомед", 25)); //2
-        people.add(new Person("Оксана", "Вячеславовна", 17));                //9
-        people.add(new Person("Владимир", "Фон Барон", 8));                  //6
+        people.add(new Person("Оксана", "Вячеславовна", 13));                //9 will be removed
+        people.add(new Person("Владимир", "Фон Барон", 8));                  //6 will be removed
 
         Collections.sort(people, new PersonWordsSurnameComparator(3));
+        System.out.println(people);
 
+        people.removeIf(Person -> Person.getAge() < 18);
         System.out.println(people);
     }
 }
